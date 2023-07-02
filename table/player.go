cdaae74ded89f6fdbf8ad0adccde2a5f88455fb1
Fred Chien <cfsghost@gmail.com>
2023-07-03 00:39:01 +0800

    move player out into individual file

---
+++ table/player.go
@@ -0,0 +1,31 @@
+package table
+
+type PlayerInfo struct {
+	ID        string   `json:"id"`
+	SeatID    int      `json:"seat_id"`
+	GameIdx   int      `json:"game_idx"`
+	Positions []string `json:"positions"`
+	Bankroll  int64    `json:"bankroll"`
+}
+
+func (pi *PlayerInfo) CheckPosition(pos string) bool {
+
+	for _, position := range pi.Positions {
+		if pos == position {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (pi *PlayerInfo) Assign(pos string) {
+
+	for _, position := range pi.Positions {
+		if pos == position {
+			return
+		}
+	}
+
+	pi.Positions = append(pi.Positions, pos)
+}
--- table/state.go
+++ table/state.go
@@ -40,33 +40,3 @@ func (s *State) ResetPositions() {
 		p.Positions = make([]string, 0)
 	}
 }
-
-type PlayerInfo struct {
-	ID        string   `json:"id"`
-	SeatID    int      `json:"seat_id"`
-	GameIdx   int      `json:"game_idx"`
-	Positions []string `json:"positions"`
-	Bankroll  int64    `json:"bankroll"`
-}
-
-func (pi *PlayerInfo) CheckPosition(pos string) bool {
-
-	for _, position := range pi.Positions {
-		if pos == position {
-			return true
-		}
-	}
-
-	return false
-}
-
-func (pi *PlayerInfo) Assign(pos string) {
-
-	for _, position := range pi.Positions {
-		if pos == position {
-			return
-		}
-	}
-
-	pi.Positions = append(pi.Positions, pos)
-}
